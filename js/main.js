c65585d3ce0c694541f5686eda0355c7001037ef
spencer kyonka <spencer.kyonka@hotmail.ca>
2017-12-21 16:36:42 -0500

    done light switch

--- js/main.js
+++ js/main.js
@@ -1,2 +1,29 @@
+console.log('Above EventListener')
+
+document.addEventListener ('DOMContentLoaded', function() {
+  console.log('The DOM is ready!')
 
 
+  var buttoner = document.querySelector('button'),
+      darkness = document.querySelector('.light'),
+      statusTexter = document.querySelector('.status');
+
+  buttoner.addEventListener('click', function() {
+    if (buttoner.className === 'switch on') {
+      buttoner.className = 'switch off';
+      darkness.className = 'dark';
+      statusTexter.innerText = 'Hey, who turned off the lights?';
+    } else {
+      buttoner.className = 'switch on';
+      darkness.className = 'light';
+      statusTexter.innerText = "It's so bright in here!";
+
+    }
+    console.log('It was clicked.')
+
+  });
+
+//"It's so bright in here!" when it's on, and ""
+
+});
+console.log('Below EventListener')
